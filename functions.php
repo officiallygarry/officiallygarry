4ab8aaa07f3fd75657d542de728b125e5ac7d658
Garry Hunt <44409417+officiallygarry@users.noreply.github.com>
2021-02-03 23:18:45 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -11,12 +11,12 @@ function custom_login_stylesheet() {
 add_action( 'login_enqueue_scripts', 'custom_login_stylesheet' );
 
 function my_login_logo_url() {
-    return 'https://www.jmvsolutions.co.uk';
+    return 'https://www.officiallygarry.co.uk';
 }
 add_filter('login_headerurl', 'my_login_logo_url');
 
 function my_login_logo_url_title() {
-    return 'Website design in Newton Abbot, Devon';
+    return 'The Adventures of a High Functioning Nerd';
 }
 add_filter( 'login_headertitle', 'my_login_logo_url_title' );
 
